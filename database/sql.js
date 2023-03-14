b90356f59feb6859fe4490153273716b7f1c2372
options3224@gmail.com <options3224@gmail.com>
2023-03-14 11:15:02 +0900

    MySQL2 모듈 설치 및 DB 연동(sections 테이블)

---
+++ database/sql.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(
+  process.env.JAWSDB_URL ?? {
+    host: 'localhost',
+    user: 'root',
+    database: 'yalco',
+    password: '1111',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  }
+);
+const promisePool = pool.promise();
+
+const sql = {
+
+  getSections : async () => {
+    const [rows] = await promisePool.query(`
+      SELECT * FROM sections
+    `)
+    return rows
+  },
+
+}
+
+module.exports = sql
