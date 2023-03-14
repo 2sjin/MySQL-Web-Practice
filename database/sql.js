78ef54453c49cde0b03df9810823d95587eafcd0
options3224@gmail.com <options3224@gmail.com>
2023-03-14 12:50:08 +0900

    단순 필터링 구현(섹선별 식당 목록 조회 가능)

--- database/sql.js
+++ database/sql.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool(
   process.env.JAWSDB_URL ?? {
-    host: 'localhost',
-    user: 'root',
-    database: 'yalco',
-    password: '1111',
+    host: 'localhost',      // DB 호스트의 주소(IP or Domain)
+    user: 'root',           // 접속자 이름
+    database: 'yalco',      // DB 이름
+    password: '1111',       // DB 패스워드
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -15,12 +15,34 @@ const promisePool = pool.promise();
 
 const sql = {
 
-  getSections : async () => {
-    const [rows] = await promisePool.query(`
-      SELECT * FROM sections
-    `)
+    // SQL: 섹션 테이블 전체 조회
+    getSections : async () => {
+        const [rows] = await promisePool.query(`SELECT * FROM sections`)
     return rows
-  },
+    },
+
+    // SQL: 섹션(sections) 테이블과 식당(businesses) 테이블 join 결과 조회
+    getBusinessesJoined : async (query) => {
+        const sqlQuery = `
+        SELECT * FROM sections S
+        LEFT JOIN businesses B
+            ON S.section_id = B.fk_section_id
+        WHERE TRUE
+            ${query.section
+            ? ('AND section_id = ' + query.section) : ''}
+            ${query.floor
+            ? ('AND floor = ' + query.floor) : ''}
+            ${query.status
+            ? ("AND status = '" + query.status + "'") : ''}
+        ORDER BY
+            ${query.order 
+            ? query.order : 'business_id'}
+        `
+        console.log(sqlQuery)
+
+        const [rows] = await promisePool.query(sqlQuery)
+        return rows
+    },
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@ var router = express.Router();
 
 var sql = require('../database/sql')
 
-const sectionIcons = [
-  '🍚', '🍿', '🍜', '🍣', '🥩', '☕', '🍰'
-]
+// 섹션 아이콘 목록
+const sectionIcons = ['🍚', '🍿', '🍜', '🍣', '🥩', '☕', '🍰']
 
-/* GET home page. */
+// 식당 상태 맵
+const statusKorMap = {
+  OPN: '영업중', 
+  CLS: '폐업',
+  VCT: '휴가중',
+  RMD: '리모델링'
+}
+
+// 섹션 목록
 router.get('/', async function(req, res, next) {
   const sections = await sql.getSections()
   sections.map((item) => {
@@ -17,6 +24,20 @@ router.get('/', async function(req, res, next) {
   res.render('sections', { 
     title: '섹션 목록',
     sections
+  });
+});
+
+// 섹션별 단순 식당 목록
+router.get('/biz-simple', async function(req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query)
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status]
+    return item
+  })
+
+  res.render('biz-simple', { 
+    title: '단순 식당 목록',
+    businesses
   });
 });
 
